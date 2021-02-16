adb65fbbf7d5bda677dfbc1ec7971322d5311b9e
Sean Kramer <seandominickramer@gmail.com>
2021-02-16 18:23:40 -0500

    Add endpoint to get posts for a user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -62,9 +62,14 @@ router.delete('/:id', validateUserId(), (req, res, next) => {
   .catch(next)
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId(), (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  users.getUserPosts(req.params.id)
+  .then((posts) => {
+    res.json(posts)
+  })
+  .catch(next)
 });
 
 router.post('/:id/posts', (req, res) => {
